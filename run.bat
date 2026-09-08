bbe071dfe518e208932011c7685df30514c73048
Lanfir7 <ahapxuctuk@gmail.com>
2026-09-08 03:16:14 +0300

    Publish complete 2.103.187 release payload

---
+++ run.bat
@@ -0,0 +1,8 @@
+java -Dsun.java2d.uiScale.enabled=false ^
+  -Xms512m -Xmx4g -Xss2m ^
+  -XX:+UseG1GC ^
+  -XX:SoftRefLRUPolicyMSPerMB=50 ^
+  -XX:+UseStringDeduplication ^
+  --add-exports=java.desktop/sun.awt=ALL-UNNAMED ^
+  -jar ./hafen.jar
+pause
